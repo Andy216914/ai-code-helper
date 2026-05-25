3074447487d47d7a4bbbd7b34f49beabd682650f
andy-jin <shijin21691412345@gmail.com>
2026-05-25 17:14:04 -0400

    feat: add system prompt (low-level API)

--- src/main/java/com/andy/aicodehelper/ai/AiCodeHelper.java
+++ src/main/java/com/andy/aicodehelper/ai/AiCodeHelper.java
@@ -1,6 +1,7 @@
 package com.andy.aicodehelper.ai;
 
 import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.response.ChatResponse;
@@ -15,10 +16,22 @@ public class AiCodeHelper {
     @Resource
     private ChatModel qwenChatModel;
 
+    // system prompt
+    private static final String SYSTEM_MESSAGE = """
+            You are a helpful assistant in the field of programming, helping users with questions
+            related to programming study and job interviews, and offering advice. Focus on 4 areas:
+            1. Plan clear programming learning paths
+            2. Provide project-based learning suggestions
+            3. Give a full-process guide for programmers' job hunting (e.g., resume optimization, application tips)
+            4. Share high-frequency interview questions and interview techniques
+            Please answer in clear, easy-to-understand language to help users study and find jobs efficiently.
+            """;
+
     // text prompt
     public String chat(String message){
+        SystemMessage systemMessage = SystemMessage.from(SYSTEM_MESSAGE);
         UserMessage userMessage = UserMessage.from(message);
-        ChatResponse chatResponse = qwenChatModel.chat(userMessage);
+        ChatResponse chatResponse = qwenChatModel.chat(systemMessage, userMessage);
         AiMessage aiMessage = chatResponse.aiMessage();
         log.info("AI output " + aiMessage.toString());
         return aiMessage.text();
